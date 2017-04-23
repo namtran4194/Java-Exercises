61847c1fcf3829b98a1b0d0141e506e5d9ef7fe7
Trần Xuân Nam <namtran4194@gmail.com>
2017-04-23 09:55:23 +0700

    update

--- SPOJ/src/svmc1609/HydroelectricDams.java
+++ SPOJ/src/svmc1609/HydroelectricDams.java
@@ -10,28 +10,47 @@ public class HydroelectricDams {
 		while (t-- > 0) {
 			int n = sc.nextInt();
 			int blocks[] = new int[n];
+			int currentRightMaxIdx = 0;
 			for (int i = 0; i < n; i++) {
 				blocks[i] = sc.nextInt();
+				currentRightMaxIdx = getMaxIdx(blocks, currentRightMaxIdx, i);
 			}
 
-			int min = blocks[0] < blocks[n - 1] ? blocks[0] : blocks[n - 1];
+			int currentLeftMax = -1;
 			int volumn = 0;
 			for (int i = 1; i < n - 1; i++) {
-				volumn += min - blocks[i];
+				currentLeftMax = getMax(currentLeftMax, blocks[i - 1]);
+				if (i > currentRightMaxIdx) {
+					currentRightMaxIdx = getMaxRightIdx(blocks, i + 1);
+				}
+				int minHigh = getMinHigh(currentLeftMax, blocks[currentRightMaxIdx]);
+				volumn += minHigh - blocks[i] > 0 ? minHigh - blocks[i] : 0;
 			}
-			
+
 			System.out.println(volumn);
 		}
 
 		sc.close();
 	}
-	
-	static int minHigh(int a[], int i){
-		return 0;
+
+	static int getMinHigh(int a, int b) {
+		return a < b ? a : b;
 	}
-	
-	static int maxHigh(int[] a, int i){
-		return 0;
+
+	static int getMaxRightIdx(int[] a, int i) {
+		int maxIdx = i;
+		for (int j = i + 1; j < a.length; j++) {
+			maxIdx = getMaxIdx(a, j, maxIdx);
+		}
+		return maxIdx;
+	}
+
+	static int getMax(int a, int b) {
+		return a > b ? a : b;
+	}
+
+	static int getMaxIdx(int[] a, int i, int j) {
+		return a[i] > a[j] ? i : j;
 	}
 
 }
