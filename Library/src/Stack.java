e48dcb1db41da4a14f699efd940e799ebcc5aedc
Trần Xuân Nam <namtran4194@gmail.com>
2017-07-02 14:40:02 +0700

    add new project to build Stack, Queue

---
+++ Library/src/Stack.java
@@ -0,0 +1,63 @@
+
+public class Stack<E> {
+
+	private Object[] data;
+	private int elementCount;
+	private int incrementSeed;
+
+	public Stack(int capacity, int incrementSeed) {
+		data = new Object[capacity];
+		this.incrementSeed = incrementSeed;
+		elementCount = 0;
+	}
+
+	public Stack(int capacity) {
+		this(capacity, 20);
+	}
+
+	public Stack() {
+		this(10);
+	}
+
+	public E push(E item) {
+		if (size() == data.length)
+			grow();
+		data[elementCount++] = item;
+		return item;
+	}
+
+	@SuppressWarnings("unchecked")
+	public E peek() {
+		if (isEmpty())
+			return null;
+		int index = elementCount - 1;
+		return (E) data[index];
+	}
+
+	public E pop() {
+		E obj = peek();
+		if (obj != null)
+			elementCount--;
+		return obj;
+	}
+
+	public boolean isEmpty() {
+		return elementCount == 0;
+	}
+
+	public int size() {
+		return elementCount;
+	}
+
+	private void grow() {
+		int newCapacity = data.length + incrementSeed;
+
+		Object[] oldData = new Object[data.length];
+		for (int i = 0; i < oldData.length; i++)
+			oldData[i] = data[i];
+
+		data = new Object[newCapacity];
+		for (int i = 0; i < newCapacity; i++)
+			data[i] = oldData[i];
+	}
+}
--- Other/src/other/Solution.java
+++ Other/src/other/Solution.java
@@ -1,14 +1,5 @@
 package other;
 
 public class Solution {
-	public static void main(String[] args) {
-		int a = 0;
-		for (int i = 0; i < 1000; i++) {
-			if (i % 2 == 0)
-				a += i;
-			else
-				a -= i;
-		}
-		System.out.println(a);
-	}
+
 }
