595fd5e82a0ba5af0d54719a1bda2746bf97fcb8
Trần Xuân Nam <namtran4194@gmail.com>
2017-07-10 20:46:56 +0700

    update changes

--- SPOJ/src/others/CopyingBooks.java
+++ SPOJ/src/others/CopyingBooks.java
@@ -1,47 +1,52 @@
-package other;
+package others;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedWriter;
 import java.io.InputStream;
+import java.io.OutputStreamWriter;
 
-public class Solution {
-
+public class CopyingBooks {
 	static int[] numbers;
 	static int numberOfBlocks;
-	static int result;
 
 	public static void main(String[] args) throws Exception {
-		int T = readInt(System.in);
-		for (int t = 1; t <= T; t++) {
-			int n = readInt(System.in);
-			numberOfBlocks = readInt(System.in);
+		BufferedInputStream in = new BufferedInputStream(System.in);
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+		int T = readInt(in);
+		while (T-- > 0) {
+			int n = readInt(in);
+			numberOfBlocks = readInt(in);
 
 			int sum = 0;
 			numbers = new int[n];
 			for (int i = 0; i < n; i++) {
-				numbers[i] = readInt(System.in);
+				numbers[i] = readInt(in);
 				sum += numbers[i];
 			}
 
-			Try(sum / numberOfBlocks, sum);
+			int result = Try(sum / numberOfBlocks, sum);
 
 			int temp = 0;
 			for (int i = 0; i < n; i++) {
 				temp += numbers[i];
 				if (temp > result) {
-					System.out.print("/ ");
+					writer.write("/ ");
+					writer.flush();
 					i--;
 					temp = 0;
-				} else
-					System.out.print(numbers[i] + " ");
+				} else {
+					writer.write(String.valueOf(numbers[i] + " "));
+					writer.flush();
+				}
 			}
-			System.out.println();
+			writer.write(System.lineSeparator());
+			writer.flush();
 		}
 	}
 
 	public static int readInt(InputStream in) throws Exception {
 		int result = 0;
 		boolean dig = false;
-
-		// read a byte (8bit) and transform it to number
 
 		for (int c = 0; (c = in.read()) != -1;) {
 			if (c >= '0' && c <= '9') {
@@ -54,15 +59,15 @@ public class Solution {
 		return result;
 	}
 
-	static void Try(int left, int right) {
+	static int Try(int left, int right) {
 		int mid = (left + right) / 2;
-		if (mid == left || mid == right)
-			return;
-		result = mid;
+		if (mid == left) {
+			return right;
+		}
 		if (isDivided(mid))
-			Try(left, mid);
+			return Try(left, mid);
 		else
-			Try(mid, right);
+			return Try(mid, right);
 	}
 
 	static boolean isDivided(int value) {
@@ -72,10 +77,9 @@ public class Solution {
 			if (numbers[i] > value)
 				return false;
 			sum += numbers[i];
-			if (sum >= value) {
+			if (sum > value) {
 				blocks++;
-				if (sum > value)
-					i--;
+				i--;
 				sum = 0;
 			} else if (i == numbers.length - 1 && sum != 0)
 				blocks++;
